56eb48398e34a0f7f105b8c9e39d4b9441d2ba1f
fauziabd26 <fauzi.aaf@gmail.com>
2026-04-27 13:06:57 +0700

    change db

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');       // Ganti dengan DB user cPanel
-define('DB_PASS', '');           // Ganti dengan DB password cPanel
+define('DB_USER', 'fauzi');       // Ganti dengan DB user cPanel
+define('DB_PASS', 'abdilah');           // Ganti dengan DB password cPanel
 define('DB_NAME', 'pph21_db');   // Ganti dengan nama database cPanel
 
-function getDB() {
+function getDB()
+{
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if ($conn->connect_error) {
         die(json_encode(['error' => 'Database connection failed: ' . $conn->connect_error]));
